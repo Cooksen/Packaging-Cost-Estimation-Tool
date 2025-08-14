4475ac79681a69f80cb19da85cadfb0b463e81bd
Cooksen <senchao0930@gmail.com>
2025-08-15 01:11:19 +0800

    feat: add start_app.bat to launch the Packaging Cost Estimation Tool

---
+++ start_app.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Starting Packaging Cost Estimation Tool...
+echo Please wait, a new browser window will open automatically.
+
+:: Temporarily add Python and Poetry to the PATH for this session
+set "PATH=%PATH%;C:\Program Files\Python311;%USERPROFILE%\AppData\Roaming\Python\Python311\Scripts"
+
+:: Change to the project directory
+cd "C:\Users\Sen_Chao\OneDrive - Dell Technologies\Desktop\project\cost_estimation"
+
+:: Run the application
+poetry run poe app
+
+pause
